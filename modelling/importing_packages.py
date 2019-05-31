97dec0b0044757de0ce49f114292b6c793272bcb
hivahivahiva <malekshahi.rahim@gmail.com>
2019-06-01 01:31:32 +0200

    added

--- modelling/importing_packages.py
+++ modelling/importing_packages.py
@@ -10,3 +10,9 @@ print(test_debugging(10, 2))
 
 print('test_branch_1')
 print('test_branch_1_second_change')
+
+# Revert brings back the commit.
+# You can merge branch to the master in the github side or here in pycharm
+# If you merge the master in the github side, then you can pull the master here
+
+# If you have done any changes first commit it and then do another action such as pull otr merge .
